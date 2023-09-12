4ebca8d5d0d2afce78e75b5989e9fdfbe1b44ab3
dream-of-future <130543078+dream-of-future@users.noreply.github.com>
2023-09-12 13:49:01 +0800

    Add files via upload

---
+++ 抽象库.cpp
@@ -0,0 +1,43 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+#include "³éÏó¿â.h"
+
+void my_swap(int* a, int* b)//½»»»
+{
+	*a = *a ^ *b;
+	*b = *b ^ *a;
+	*a = *b ^ *a;
+}
+
+int add(int a, int b)
+{
+	if (!a)
+		return b;
+	return add((a & b) << 1, a ^ b);
+}
+
+long long int to_bin_int(int x)
+{
+	long long int bin = 0;
+	int i = 32;
+	int tmp;
+	for (i = 32; i >= 0; i--)
+	{
+		bin *= 10;
+		tmp = (x >> i) & 1;
+		bin = bin + tmp;
+	}
+	return bin;
+}
+
+void to_bin_arr(int x, char bin[],char sz)
+{
+	char i;
+	for (i = sz; i >= 0; i--)
+	{
+		bin[sz - i] = (x >> i) & 1;
+		printf("%c", bin[sz - i] ^ 48);
+	}
+	printf("\n");
+
+}
---
+++ 抽象库.h
@@ -0,0 +1,7 @@
+#ifndef _CHOUXIANG_H_
+#define _CHOUXIANG_H_
+void my_swap(int* a, int* b);//½»»»
+int add(int a, int b);
+long long int to_bin_int(int x);//10»»2½øÖÆ
+void to_bin_arr(int x,char bin[], char sz);
+#endif
