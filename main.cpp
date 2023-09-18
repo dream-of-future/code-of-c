e4e6d09406408fcd0544aa583e1979b333400ffd
dream-of-future <1607676527@qq.com>
2023-09-18 09:56:29 +0800

    修改后使代码更规范

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+#include"ChouXiang.h"
+
+int main()
+{
+	int a = 101;
+	int b = 110;
+
+	/*my_swap(&a, &b);
+	printf("a = %d ,b = %d", a, b);*/
+
+	printf("sum = %d", add(a, b));
+
+	int c = 1;
+	printf("%d", c >> 1);
+
+	printf("\n%lld\n", to_bin_int(1024));
+	char arr[20] = {0};
+	to_bin_arr(10,arr,19);
+	for (c = 0; c < 20; c++)
+	{
+		printf("%c", *(arr + c) ^ 48);
+		//printf("%c", bin[19 - i] ^ 48);
+
+	}
+	return 0;
+}
